a54abb6b8c5abef5090cf7b25d3fb25b3aff9df8
edgarp413 <39072296+edgarp413@users.noreply.github.com>
2018-05-08 20:13:28 -0400

    Update apo.py

--- apo.py
+++ apo.py
@@ -1,5 +1,12 @@
 import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
 import asyncio
+import os
+
+
+
+bot = commands.Bot(command_prefix=".sgg")
 
 client = discord.Client()
 
