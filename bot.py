0eb31755775b64788562551648cff2f39e6772f6
edgarp413 <39072296+edgarp413@users.noreply.github.com>
2018-05-08 20:04:22 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,3 +24,16 @@ async def hi(ctx):
   
   
 bot.run(os.environ['BOT_TOKEN'])
+
+language: python
+python:
+  - "3.5.2"
+install:
+  - pip install -r requirements.txt
+script:
+  - python -m compileall ./red.py
+  - python -m compileall ./cogs
+  - python ./red.py --no-prompt --no-cogs --dry-run
+cache: pip
+notifications:
+  email: false
